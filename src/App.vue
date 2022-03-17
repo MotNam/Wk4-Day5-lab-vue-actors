da727c806c123db6607459fa27a98e21d94e1f43
MotNam <motokonamba@gmail.com>
2022-03-18 00:58:57 +0100

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,61 @@
 <script setup>
-import { ref } from "vue"
+
+import { ref, reactive  } from "vue"
 
 import contacts from "./contacts.json";
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
 
-const myContacts = contacts;
-// myContacts.splice(15);
+let myContacts = contacts;
+let listOfFive = myContacts.splice(5);
 // console.log(myContacts);
 
 const wonOscar = ref(true);
 const wonEmmy = ref(true);
 
+function addRandom(){
+  let randomContacts = this.myContacts.length - this.listOfFive.length;
+
+  let chooseOne = (Math.random()*randomContacts.length) * randomContacts.length;
+  
+  console.log(chooseOne.value)
+    
+};
  
+let  newList = listOfFive += chooseOne;
+
+// evenNumbers: function () {
+//     return this.numbers.filter(function (number) {
+//       return number % 2 === 0
+//     })
+//   }
+
+// evenNumbers: function () {
+//     return this.numbers.sort(function (number) {
+//       return number % 2 === 0
+//     })
+//   }
+
 </script>
 
 <template>
+
+
+
 <div class="actors-list">
   <caption>IronContacts</caption>
+ 
+  <button @click="addRandom">Add New Contact 
+</button>
+{{newList.value}}
+
+<button @click="sortByPopul">Sort by Popularity 
+</button>
+
+<button @click="sortByName">Sort by Name
+</button>
+
+
   <td>
     <th>Picture</th>
 
@@ -38,15 +75,19 @@ const wonEmmy = ref(true);
  <td><img class="pics" :src="contact.pictureUrl" alt="foto"></td>
   <td> {{ contact.name }}</td>
   <td> {{ contact.popularity.toFixed(2) }}</td>
+ 
   <td v-if="contact.wonOscar"> Won Oscar 🏆 </td>
   <td v-else > </td>
+ 
   <td v-if="contact.wonEmmy">Won Emmy </td>
  <td v-else > </td>
 
-<button @click="addRandom"></button>
+<td>{{chooseOne.contact.name}}</td>
+<td>{{chooseOne.contact.popularity}}</td>
+
   </div>
 
-  <img alt="Vue logo" src="./assets/logo.png" />
+  
 
 </div>
 
